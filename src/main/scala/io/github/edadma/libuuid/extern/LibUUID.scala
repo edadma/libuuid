c47ca040755a410a7ceae0132414b1a84c1e776f
Edward A Maxedon Sr <edadma@gmail.com>
2023-03-18 09:30:13 -0400

    add parse()

--- src/main/scala/io/github/edadma/libuuid/extern/LibUUID.scala
+++ src/main/scala/io/github/edadma/libuuid/extern/LibUUID.scala
@@ -5,8 +5,8 @@ import scala.scalanative.unsafe._, Nat._
 @link("uuid")
 @extern
 object LibUUID:
-  type uuid_t = CArray[CUnsignedChar, Digit2[_1, _6]]
-  type uuid_tp = Ptr[uuid_t]
+  type uuid_tp = CArray[CUnsignedChar, Digit2[_1, _6]]
+  // type uuid_tp = Ptr[uuid_t]
 
   def uuid_generate_random(out: uuid_tp): Unit = extern
   def uuid_unparse(uu: uuid_tp, out: CString): Unit = extern
--- src/main/scala/io/github/edadma/libuuid/package.scala
+++ src/main/scala/io/github/edadma/libuuid/package.scala
@@ -1,6 +1,8 @@
 package io.github.edadma.libuuid
 
 import scala.scalanative.unsafe._
+import scala.scalanative.unsigned._
+
 import io.github.edadma.libuuid.extern.LibUUID._
 
 implicit class Type(val value: CInt) extends AnyVal
@@ -13,16 +15,16 @@ object Type:
   final val RANDOM = new Type(4)
   final val SHA1 = new Type(5)
 
-class UUID private (val arr: Array[Byte]):
+class UUID private[libuuid] (val arr: Array[Byte]):
   private def toUUID(uu: uuid_tp): Unit =
     var i = 0
 
     while i < 16 do
-      uu(i) = arr(i)
+      uu(i) = arr(i).toUByte
       i += 1
 
   def typ: Type =
-    val uuid = stackalloc[uuid_t]()
+    val uuid = stackalloc[uuid_tp]()
 
     toUUID(uuid)
     uuid_type(uuid)
@@ -32,13 +34,13 @@ private def fromUUID(uu: uuid_tp): UUID =
   var i = 0
 
   while i < 16 do
-    arr(i) = uu(i)
+    arr(i) = uu(i).toByte
     i += 1
 
   new UUID(arr)
 
 def generateRandomUnparse: String =
-  val binuuid = stackalloc[uuid_t]()
+  val binuuid = stackalloc[uuid_tp]()
   val uuid = stackalloc[CChar](37)
 
   uuid_generate_random(binuuid)
@@ -46,13 +48,13 @@ def generateRandomUnparse: String =
   fromCString(uuid)
 
 def generateRandom: UUID =
-  val binuuid = stackalloc[uuid_t]()
+  val binuuid = stackalloc[uuid_tp]()
 
   uuid_generate_random(binuuid)
   fromUUID(binuuid)
 
 def parse(uu: String): Option[UUID] =
-  val binuuid = stackalloc[uuid_t]()
+  val binuuid = stackalloc[uuid_tp]()
 
-  if Zone(implicit z => uuid_parse(toCString(uu) binuuid)) != 0 then None
+  if Zone(implicit z => uuid_parse(toCString(uu), binuuid)) != 0 then None
   else Some(fromUUID(binuuid))
