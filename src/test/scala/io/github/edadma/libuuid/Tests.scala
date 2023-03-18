a3fdcce72786cba023af448f95f22664b442189a
Edward A Maxedon Sr <edadma@gmail.com>
2023-03-18 10:46:26 -0400

    unit tests

--- src/test/scala/io/github/edadma/libuuid/Tests.scala
+++ src/test/scala/io/github/edadma/libuuid/Tests.scala
@@ -5,12 +5,30 @@ import org.scalatest.matchers.should.Matchers
 
 class Tests extends AnyFreeSpec with Matchers {
 
-  "test" in {
-    List(1, 2, 3) mkString "\n" shouldBe
-      """|1
-         |2
-         |3
-      """.trim.stripMargin
+  "parse" in {
+    parse("433afe80-8b40-41dc-9bde-61134946989e") shouldNot be(None)
+  }
+
+  "Type.RANDOM" in {
+    parse("433afe80-8b40-41dc-9bde-61134946989e").get.typ shouldBe Type.RANDOM
+  }
+
+  "parse fail" in {
+    parse("bad UUID") shouldBe None
+  }
+
+  "generateRandomString/parse/unparse" in {
+    val u = generateRandomString
+    val p = parse(u).get
+
+    p.unparse shouldBe u
+  }
+
+  "generateRandomString/typ" in {
+    val u = generateRandomString
+    val p = parse(u).get
+
+    p.typ shouldBe Type.RANDOM
   }
 
 }
